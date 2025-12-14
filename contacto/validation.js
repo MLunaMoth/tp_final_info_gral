5be4abf5386d1057dad49dddc7ca1e24582975f9
MLunaMoth <micaelamonti@gmail.com>
2025-12-14 09:02:33 -0300

    formulario completo provar envio en github pages

---
+++ contacto/validation.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+    // Seleccionamos todos los formularios con la clase needs-validation
+    var forms = document.querySelectorAll('.needs-validation');
+
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+})();
